1e06ae2531cf603afa63055fe4846e3a1d7b8d48
Sajjadkha2221 <sajjaduetpeshawar@gmail.com>
2026-09-07 12:34:15 +0500

    pull and push

--- third_file.py
+++ third_file.py
@@ -4,3 +4,5 @@ this is another commit for checking.
 another commit okay.
 edited in gui
 trying pull command.
+whenever when you edit a file on local machine use push commands to upload contents and when you edited the file on github  
+we will use pull command to get the edited version on local machine.
